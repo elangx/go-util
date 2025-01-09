a75617672908d530622584f3fe1c2f3c6a4ec0f9
yangxian <yangxian@miliantech.com>
2025-01-09 20:40:36 +0800

    add country code to emoji

--- tool/tool.go
+++ tool/tool.go
@@ -1,5 +1,10 @@
 package tool
 
+import (
+	"strings"
+	"unicode"
+)
+
 // 类三元运算符
 func IF[T any](condition bool, trueVal, falseVal T) T {
 	if condition {
@@ -7,4 +12,27 @@ func IF[T any](condition bool, trueVal, falseVal T) T {
 	} else {
 		return falseVal
 	}
+}
+
+// 国家码转换成对应国家的国旗emoji
+func CountryCodeToEmoji(countryCode string) string {
+	if len(countryCode) != 2 {
+		return ""
+	}
+
+	//台湾要转成CN，否则会出民国旗
+	if strings.ToUpper(countryCode) == "TW" {
+		countryCode = "CN"
+	}
+
+	var emoji string
+	for _, r := range countryCode {
+		//保证只有英文字母
+		if !unicode.IsLetter(r) {
+			return ""
+		}
+		// 计算Regional Indicator Symbol的Unicode码点
+		emoji += string(unicode.ToUpper(r) + 0x1F1E6 - 'A')
+	}
+	return emoji
 }
